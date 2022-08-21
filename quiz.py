ecf4682b082954dde12523a7d8455c45f00a0f94
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2022-08-21 16:11:57 -0400

    Add files via upload
    Quiz game about computer parts

---
+++ quiz.py
@@ -0,0 +1,42 @@
+print("Welcome to my computer quiz")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+    
+print("Okay! Let's play :)")
+score = 0
+
+#questions
+anwser = input("What does CPU stand for? ")
+if anwser.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+    
+anwser = input("What does GPU stand for? ")
+if anwser.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+anwser = input("What does RAM stand for? ")
+if anwser.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+anwser = input("What does PSU stand for? ")
+if anwser.lower() == "power supply unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+
+
